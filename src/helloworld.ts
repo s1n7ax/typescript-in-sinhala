5b1c1916491f115c12c886ce27690a722aee6d5a
s1n7ax <srineshnisala@gmail.com>
2020-03-31 06:19:43 +0530

    add hello world script

---
+++ src/helloworld.ts
@@ -0,0 +1,9 @@
+export default class Main {
+	static applicationName: string = 'Demo';
+
+	static printApplicationName() {
+		console.log(Main.applicationName);
+	}
+}
+
+Main.printApplicationName();
