bb05a6293d8c227ef91d9f696cd98f2971e9ade6
s1n7ax <srineshnisala@gmail.com>
2020-04-18 15:20:48 +0530

    [ADD] add functions and classes

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,26 @@
-const _boolean: boolean = true;
-console.log(`_boolean :: ${typeof _boolean}`);
+const printName = function (name: string): void {
+    console.log(name);
+};
 
-const _number: number = 20;
-console.log(`_number :: ${typeof _number}`);
+class Cat {
+    static className = 'Cat';
+    name: string;
 
-const _string: string = 'string';
-console.log(`_string :: ${typeof _string}`);
+    constructor(name: string) {
+        this.name = name;
+    }
 
-const array1: Array<number> = [1, 3];
-const array2: number[] = [1, 3];
-console.log(`array1 :: ${typeof array1}`);
-console.log(`array2 :: ${typeof array2}`);
+    static printClassName() {
+        console.log(Cat.className);
+    }
 
-const _tuple: [string, boolean] = ['hello', true];
-console.log(`_tuple :: ${typeof _tuple}`);
-
-enum Color {
-    RED,
-    BLUE,
-    GREEN,
+    printName() {
+        console.log(this.name);
+    }
 }
-const color: Color = Color.GREEN;
-console.log(`Color :: ${typeof color}`);
 
-const _null: null = null;
-console.log(`_null :: ${typeof _null}`);
+printName('Srinesh');
 
-const _undifined: undefined = undefined;
-console.log(`_undifined :: ${typeof _undifined}`);
-
-const _object: object = {};
-console.log(`_object :: ${typeof _object}`);
-
-const _any: any = [1, 'string', undefined, Color.BLUE];
-console.log(`_any :: ${typeof _any}`);
+const c = new Cat('garfield');
+c.printName();
+Cat.printClassName();
